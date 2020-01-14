45859240f560d509ea998b8ea1a327fa7f079b97
jbender <jdbender64@gmail.com>
2020-01-14 09:22:38 -0800

    converts hybrid-element with flags

---
+++ hybrid-element.js
@@ -0,0 +1,14 @@
+import '@polymer/paper-input/paper-input.js';
+import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
+import { html } from '@polymer/polymer/lib/utils/html-tag.js';
+Polymer({
+  _template: html`
+        <paper-input value="hybrid!!!"></paper-input>
+`,
+
+  is: "hybrid-element",
+
+  properties: {
+
+  }
+});
